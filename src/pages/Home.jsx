8c8ad5d8d17fce90c1f10d62d875ea99302cd6b9
bhatqayoom <abdul.qayoom@bqe.com>
2026-01-28 14:36:27 +0530

    removed customer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import SEO from '../components/SEO'
-import Hero from '../components/Hero'
-import Categories from '../components/Categories'
-import GiftBoxes from '../components/GiftBoxes'
-import Features from '../components/Features'
-import Testimonials from '../components/Testimonials'
-import Newsletter from '../components/Newsletter'
-import FeaturedProducts from '../components/FeaturedProducts'
-import SignatureGiftBoxes from '../components/SignatureGiftBoxes'
-import './Home.css'
+import SEO from "../components/SEO";
+import Hero from "../components/Hero";
+import Categories from "../components/Categories";
+import GiftBoxes from "../components/GiftBoxes";
+import Features from "../components/Features";
+import Testimonials from "../components/Testimonials";
+import Newsletter from "../components/Newsletter";
+import FeaturedProducts from "../components/FeaturedProducts";
+import SignatureGiftBoxes from "../components/SignatureGiftBoxes";
+import "./Home.css";
 
 const Home = () => {
   return (
     <main>
-      <SEO 
+      <SEO
         title="Kashmir Dry Delight - Premium Dry Fruits & Nuts Online Store"
         description="Buy premium quality dry fruits, nuts, dates, berries, and gift boxes from Kashmir. 100% quality guarantee, fast shipping across India. Shop now!"
         keywords="dry fruits, nuts, almonds, cashews, dates, berries, kashmir dry fruits, premium nuts, gift boxes, online dry fruits"
@@ -22,13 +22,12 @@ const Home = () => {
       <Categories />
       <GiftBoxes />
       <Features />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Newsletter />
       <FeaturedProducts />
       <SignatureGiftBoxes />
     </main>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
